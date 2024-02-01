fac0c6f9a5dd2206be5972d35a42fb2318a05e9d
azizbek400 <aziznormurodov400@gmail.com>
2024-02-01 05:22:25 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// for1  /////////////////
+
+// let k = 5;
+// let n = 10;
+// for (let i = 1; i <= n; i++) {
+//     console.log(k);
+// }
+
+
+// for 1  /////////////////////////
+// let a = 4;
+// let b = 23;
+// for (let i = a; i <=b ; i++) {
+//     console.log(i); 
+//
+
+// for 4  /////////////////////////////
+// for ( let i = 1.2; i < 2 ; i += 0.2) {
+//     console.log(i);
+// }
+
+
+
+// for 2  //////////////////////////////
+// let a = 4;
+// let b = 23;
+// for (let i = b; i >a ; i--) {
+//     console.log(i); 
+// }
